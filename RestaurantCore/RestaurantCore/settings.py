6411a7096d3ad23485c9be71bc4a31149f568ba7
sravanpant <sravan.pant5@gmail.com>
2024-08-01 21:52:32 +0530

    made changes to settings.py

--- RestaurantCore/RestaurantCore/settings.py
+++ RestaurantCore/RestaurantCore/settings.py
@@ -143,10 +143,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATIC_URL = "/static/"
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
